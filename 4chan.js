4a7146af00aa0e5ef469d4e41ef86577bef9f464
SK_Artemis <98838698+ArK5ZER0@users.noreply.github.com>
2023-10-14 08:51:40 +0200

    Update 4chan.js

--- 4chan.js
+++ 4chan.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    const url = "www.4chan.org";
+    const url = "https://www.4chan.org/";
     const iframe = `<iframe src="${url}" height=1024 width=1280></iframe>`;
     ns.alert(iframe);
 }
