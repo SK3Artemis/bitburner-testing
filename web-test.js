4a2844ab35b6e2175ba1af0800eda9d65cd1da3f
SK_Artemis <98838698+ArK5ZER0@users.noreply.github.com>
2023-10-14 08:22:07 +0200

    web-test.js

--- web-test.js
+++ web-test.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
     const url = "https://copy.sh/";
-    const iframe = `<iframe src="${url}" height=1024 width=1600></iframe>`;
+    const iframe = `<iframe src="${url}" height=1024 width=1280></iframe>`;
     ns.alert(iframe);
 }
