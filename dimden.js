8679fbe6e6fe6063a540fd8d500a73262964fc79
SK_Artemis <98838698+SK3Artemis@users.noreply.github.com>
2024-11-04 06:54:51 +0100

    Delete dimden.js

--- dimden.js
+++
@@ -1,6 +0,0 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    const url = "https://dimden.dev/";
-    const iframe = `<iframe src="${url}" height=1024 width=1280></iframe>`;
-    ns.alert(iframe);
-}
