f457b8017bc0efc7339c66b34c2f2a9f90156d3e
SK_Artemis <rvl.test.srg@gmail.com>
2025-07-27 20:00:03 +0200

    Update bitburner-web.js

--- test/bitburner-web.js
+++ test/bitburner-web.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    const url = "https://danielyxie.github.io/bitburner/";
+    const url = "https://bitburner-official.github.io";
     const iframe = `<iframe src="${url}" height=1024 width=1280></iframe>`;
     ns.alert(iframe);
 }
